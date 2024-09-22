db817c05987026ffeb4ff9539d400689798b1f34
Sibghat Al Baloshi <sibghatdev@gmail.com>
2024-09-23 01:22:36 +0500

    new day new changes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -22,21 +22,93 @@ function App() {
         </div>
       </div>
 
-
       <div className="portfolio">
 
-      <div class="four">
-          <h1><span>PORTFOLIO</span> WEBSITES I <em>MADE</em></h1>
+        <div id="container">
+
+          <div className="product-details">
+
+            <h1>SIBGHAT IDEAS</h1>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum optio ad deleniti saepe enim perspiciatis necessitatibus cupiditate ratione! Asperiores officiis tenetur dolore expedita consequatur minima explicabo provident recusandae vero accusantium! lorem0500</p>
+
+
+            <div className="control">
+
+              <button className="btn">
+                <span className="price">WEB</span>
+                <span className="shopping-cart"><i className="fa fa-arrow-right" aria-hidden="true"></i></span>
+                <span className="buy">REDIRECT</span>
+              </button>
+
+            </div>
+
+          </div>
+
+          <div className="product-image">
+
+            <img src={SibghatIdeas} alt=""/>
+
+
+              <div className="information">
+                <h2> TECHNOLOGIES</h2>
+                <ul>
+                  <li><strong>JAVASCRIPT  </strong> </li>
+                  <li><strong>HTML  </strong></li>
+                  <li><strong>CSS </strong></li>
+                  <li><strong>REACT </strong></li>
+
+                </ul>
+              </div>
+
+          </div>
+        </div>
+
       </div>
-        
-        <div className="card" data-aos="fade-up">
-          <img src={SibghatIdeas} alt="SIBGHAT_IDEAS_LOOK" />
-          <section className="description">
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa, eaque? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius deserunt veritatis itaque quas voluptates. Soluta accusantium ab quaerat provident fugit!</p>
-            <button>read more.</button>
-          </section>
-         </div>
+      <div className="portfolio">
+
+<div id="container">
+
+  <div className="product-details">
+
+    <h1>SIBGHAT IDEAS</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum optio ad deleniti saepe enim perspiciatis necessitatibus cupiditate ratione! Asperiores officiis tenetur dolore expedita consequatur minima explicabo provident recusandae vero accusantium! lorem0500</p>
+
+
+    <div className="control">
+
+      <button className="btn">
+        <span className="price">WEB</span>
+        <span className="shopping-cart"><i className="fa fa-arrow-right" aria-hidden="true"></i></span>
+        <span className="buy">REDIRECT</span>
+      </button>
+
+    </div>
+
+  </div>
+
+  <div className="product-image">
+
+    <img src={SibghatIdeas} alt=""/>
+
+
+      <div className="information">
+        <h2> TECHNOLOGIES</h2>
+        <ul>
+          <li><strong>JAVASCRIPT</strong></li>
+          <li><strong>CSS</strong></li>
+          <li><strong>HTML</strong></li>
+          <li><strong>REACT</strong></li>
+          <li><strong>REDUX</strong></li>
+
+        </ul>
       </div>
+
+  </div>
+</div>
+
+</div>
 
 
     </div>
