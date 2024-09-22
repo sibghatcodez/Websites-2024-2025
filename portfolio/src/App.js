5552334e3cc93feed65481165046f9adf8a5a2fb
Sibghat Al Baloshi <sibghatdev@gmail.com>
2024-09-22 18:43:58 +0500

    new  changes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
-import Sibs from './sibghat.png';
+import CV from './WebCV.png';
+import WEBSITES from './WEBSITES.jpg'
+import SibghatIdeas from './WebsitesPictures/sibghatIdeas.jpg';
 
 function App() {
   return (
@@ -10,40 +12,30 @@ function App() {
           <p id="craft">GAME DEVELOPER</p>
           <h2 id='me'>I'm Sibghat Ullah</h2>
           <div className="buttons">
-            <button id="about_me">PORTFOLIO</button>
-            <button id="hire_me"><a href="https://www.linkedin.com/in/sibghat-ullah-84a66321b/" target="_blank">HIRE ME</a></button>
+            <button id="about_me" className="hvr-shadow-radial hvr-buzz">PORTFOLIO</button>
+            <button id="hire_me" className="hvr-shadow-radial hvr-buzz-out"><a href="https://www.linkedin.com/in/sibghat-ullah-84a66321b/" target="_blank">HIRE ME</a></button>
           </div>
         </div>
-        {/* <div className="image">
-        <img src={Sibs} alt="My_Picture" />
-      </div> */}
+
         <div className="intro">
-
-            <h1>Education</h1>
-          <div className="education">
-            <p><span id="period">2011-2022</span> Matriculation of Science from Board of Secondary Education</p>
-            &nbsp;
-            <p><span id="period">2022-2024</span> Inter of Computer Science from Board of Intermediate</p>
-            &nbsp;
-            <p><span id="period">2024-present</span> Bachelor of Computer Science from University of Sindh</p>
-          </div>
-
-            <h1>Skills</h1>
-          <div className="skills">
-            <p>Game Development</p>
-            &nbsp;
-            <p>Web Development</p>
-            &nbsp;
-            <p>Git Version Control</p>
-          </div>
-
-            <h1>Softwares</h1>
-          <div className="softwares">
-            <p>Unity Engine</p>
-            &nbsp;
-            <p>VS Code</p>
-          </div>
+          <img src={CV} alt="intro_picture" />
         </div>
+      </div>
+
+
+      <div className="portfolio">
+
+      <div class="four">
+          <h1><span>PORTFOLIO</span> WEBSITES I <em>MADE</em></h1>
+      </div>
+        
+        <div className="card" data-aos="fade-up">
+          <img src={SibghatIdeas} alt="SIBGHAT_IDEAS_LOOK" />
+          <section className="description">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa, eaque? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius deserunt veritatis itaque quas voluptates. Soluta accusantium ab quaerat provident fugit!</p>
+            <button>read more.</button>
+          </section>
+         </div>
       </div>
 
 
