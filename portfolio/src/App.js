027a682fa9901093591a68cfca73a7c66f1db370
Sibghat Al Baloshi <sibghatdev@gmail.com>
2024-09-27 18:42:31 +0500

    finally done

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -21,6 +21,10 @@ function App() {
       technologies: "HTML, CSS, REACT, JAVASCRIPT, REDUX, FIREBASE",
     }
   ]
+  const gamesArray = [
+  ]
+ 
+
   function getList(website) {
     return websiteArray[website].technologies.split(",");
   }
@@ -30,6 +34,13 @@ function App() {
     //portfolio class is to showcase websites and games made.
     //construction
     <div className="main">
+
+      {
+        websiteArray.map((web, index) => {
+          console.log(index, web.name);
+        })
+      }
+
       <div className="container">
 
         <div className="info">
@@ -49,81 +60,89 @@ function App() {
       <h1 id="WebsiteHeading">WEBSITES THAT I MADE</h1>
       <div className="portfolio">
 
-        <div id="container">
+        {websiteArray.length > 0 && websiteArray.map((website, index) => (
+          <div id="container" key={index}>
 
-          <div className="website-details">
+            <div className="website-details">
 
-            <h1>Sibghat Ideas</h1>
+              <h1>{website.name}</h1>
 
-            <p>SibghatIdeas is a learning platform designed to teach HTML in Urdu, making it easier for students to grasp web development concepts. It simplifies English documentation into an accessible format for everyone.</p>
+              <p>{website.introduction}</p>
 
 
-            <div className="control">
+              <div className="control">
 
-              <button className="btn"><a href="https://sibghatideas.netlify.app/" target="_blank">
-                <span className="tag">WEB</span>
-                <span className="redirect"><i className="fa fa-arrow-right" aria-hidden="true"></i></span>
-                <span className="redirect-btn">REDIRECT</span>
-              </a></button>
+                <button className="btn"><a href={website.link} target="_blank">
+                  <span className="tag">WEB</span>
+                  <span className="redirect"><i className="fa fa-arrow-right" aria-hidden="true"></i></span>
+                  <span className="redirect-btn">REDIRECT</span>
+                </a></button>
 
+              </div>
+            </div>
+
+            <div className="website-image">
+
+              <img src={website.image} alt="" />
+
+
+              <div className="information">
+                <h2> TECHNOLOGIES</h2>
+                <ul>
+                  {getList(index).map((tech, i) => (
+                    <li key={i}><strong>{tech.trim()}</strong></li>
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
+        ))};
+         {websiteArray.length == 0 &&
+          <h1 id='nothing_to_show'>NO WEBSITES MADE TO SHOW AS OF YET.</h1>
+        }
+      </div>
+      <h1 id="WebsiteHeading">GAMES THAT I MADE</h1>
+      <div className="portfolio">
+        {gamesArray.length > 0 && gamesArray.map((game, index) => (
+           <div id="container" key={index}>
 
-          <div className="website-image">
+           <div className="website-details">
 
-            <img src={SibghatIdeas} alt="" />
+             <h1>{game.name}</h1>
 
-
-            <div className="information">
-              <h2> TECHNOLOGIES</h2>
-              <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>REACT</li>
-                <li>JAVASCRIPT</li>
-              </ul>
-            </div>
-          </div>
-        </div>
-
-        <div id="container">
-
-          <div className="website-details">
-
-            <h1>Baloch Squad</h1>
-
-            <p>BalochSquad is a platform where we document our cricket matches. From match locations and dates to overs, scores, and wickets, we keep track of every detail as we compete with other teams.</p>
+             <p>{game.introduction}</p>
 
 
-            <div className="control">
+             <div className="control">
 
-              <button className="btn"><a href="https://balochsquad.netlify.app/" target="_blank">
-                <span className="tag">WEB</span>
-                <span className="redirect"><i className="fa fa-arrow-right" aria-hidden="true"></i></span>
-                <span className="redirect-btn">REDIRECT</span>
-              </a></button>
+               <button className="btn"><a href={game.link} target="_blank">
+                 <span className="tag">WEB</span>
+                 <span className="redirect"><i className="fa fa-arrow-right" aria-hidden="true"></i></span>
+                 <span className="redirect-btn">REDIRECT</span>
+               </a></button>
 
-            </div>
-          </div>
+             </div>
+           </div>
 
-          <div className="website-image">
+           <div className="website-image">
 
-            <img src={BalochSquad} alt="" />
+             <img src={game.image} alt="" />
 
 
-            <div className="information">
-              <h2> TECHNOLOGIES</h2>
-              <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>REACT</li>
-                <li>JAVASCRIPT</li>
-                <li>FIREBASE</li>
-                <li>REDUX</li>
-              </ul>
-            </div>
-          </div>
-        </div>
+             <div className="information">
+               <h2> TECHNOLOGIES</h2>
+               <ul>
+                 {getList(index).map((tech, i) => (
+                   <li key={i}><strong>{tech.trim()}</strong></li>
+                 ))}
+               </ul>
+             </div>
+           </div>
+         </div>
+        ))}
+        {gamesArray.length == 0 &&
+          <h1 id='nothing_to_show'>NO GAMES MADE TO SHOW AS OF YET.</h1>
+        }
       </div>
     </div>
   );
